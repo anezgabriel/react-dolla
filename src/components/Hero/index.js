001626f41b28b9b9c643168c6ce4ce9faf2feee2
Gabriel Añez <artu242@gmail.com>
2021-08-01 18:04:07 -0400

    Home Hero Section and Button Element

---
+++ src/components/Hero/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import {
+  ArrowForward,
+  ArrowRight,
+  HeroBg,
+  HeroBtnWrapper,
+  HeroContainer,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  VideoBg
+} from './HeroElements';
+import video from '../../videos/hero.mp4';
+import { Button } from '../ButtonElement';
+
+const Hero = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover((currentVal) => !currentVal);
+  };
+
+  return (
+    <HeroContainer id="home">
+      <HeroBg>
+        <VideoBg
+          autoPlay={true}
+          loop={true}
+          controls={false}
+          playsInline
+          src={video}
+          type="video/mp4"
+        />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Virtual Banking Made Easy</HeroH1>
+        <HeroP>
+          Register for a new account today and receive $250 in credit towards
+          your next payment.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            to="register"
+            primary
+            dark>
+            Get started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default Hero;
