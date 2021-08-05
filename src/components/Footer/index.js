83dbbdd770a68b5fd12d51a44e598b5fb4bf540e
Gabriel Añez <artu242@gmail.com>
2021-08-05 13:07:49 -0400

    Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import {
+  Copyright,
+  CopyrightWrap,
+  FooterContainer,
+  FooterLink,
+  FooterLinkItems,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkTitle,
+  FooterWrap,
+  WebsiteLogo,
+  WebsiteRights
+} from './FooterElements';
+
+const fakeLinks = [
+  {
+    title: 'About Us',
+    links: [
+      'How it works',
+      'Testimonials',
+      'Careers',
+      'Investors',
+      'Terms of Service'
+    ]
+  },
+  {
+    title: 'Videos',
+    links: ['Submit Video', 'Ambassadors', 'Agency', 'Influencer']
+  },
+  {
+    title: 'Contact Us',
+    links: ['Contact', 'Support', 'Destinations', 'Sponsorships']
+  },
+  {
+    title: 'Social Media',
+    links: ['Instagram', 'Facebook', 'Youtube', 'Twitter']
+  }
+];
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            {fakeLinks.map((el) => (
+              <FooterLinkItems key={el.title}>
+                <FooterLinkTitle>{el.title}</FooterLinkTitle>
+                {el.links.map((link) => (
+                  <FooterLink key={link} to="/login">
+                    {link}
+                  </FooterLink>
+                ))}
+              </FooterLinkItems>
+            ))}
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+        <Copyright>
+          <CopyrightWrap>
+            <WebsiteLogo to="/">dolla</WebsiteLogo>
+            <WebsiteRights>
+              dolla © {new Date().getFullYear()} All rights reserved.
+            </WebsiteRights>
+          </CopyrightWrap>
+        </Copyright>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Footer from '../components/Footer';
 import Hero from '../components/Hero';
 import InfoSection from '../components/InfoSection';
 import {
@@ -26,6 +27,7 @@ const Home = () => {
       <InfoSection {...HomeObjTwo} />
       <Services />
       <InfoSection {...HomeObjThree} />
+      <Footer />
     </>
   );
 };
