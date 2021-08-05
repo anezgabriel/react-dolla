eb8fedd7ffb736f66aaf82c96646f8979bb1c7d4
Gabriel Añez <artu242@gmail.com>
2021-08-05 12:18:03 -0400

    Added Routes and Login Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages';
+import LoginPage from './pages/login';
 
 function App() {
   return (
     <BrowserRouter>
-      <Home />
+      <Switch>
+        <Route path="/login" exact component={LoginPage} />
+        <Route path="/" exact component={Home} />
+      </Switch>
     </BrowserRouter>
   );
 }
